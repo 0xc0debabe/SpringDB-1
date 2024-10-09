b3bc531fa7e5d019a58308659c104c9f00da3e42
0xc0debabe <kongminoo@naver.com>
2024-10-09 18:48:19 +0900

    [24/10/09] Connection Pool

---
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV1.java
@@ -0,0 +1,126 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+@Slf4j
+public class MemberRepositoryV1 {
+
+    private final DataSource dataSource;
+
+    public MemberRepositoryV1(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public Member save(Member member) throws SQLException{
+        String sql = "INSERT INTO Member(member_id, money) values(?, ?)";
+
+        Connection con = null;
+        PreparedStatement ps = null;
+
+        try {
+            con = getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, member.getMemberId());
+            ps.setInt(2, member.getMoney());
+            ps.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            log.error("db error");
+            throw e;
+        } finally {
+            close(con, ps, null);
+        }
+    }
+
+    public Member findById(String memberId) throws SQLException{
+        String sql = "select * from member where member_id = ?";
+
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            connection = getConnection();
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, memberId);
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId = " + memberId);
+            }
+
+        } catch (SQLException e) {
+            log.error("db error = ", e);
+            throw e;
+        } finally {
+            close(connection, ps, rs);
+        }
+    }
+
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "UPDATE member SET money = ? WHERE member_id = ?";
+
+        Connection con = null;
+        PreparedStatement ps = null;
+
+        try {
+            con = getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, money);
+            ps.setString(2, memberId);
+            int resultSize = ps.executeUpdate();
+            log.info("resultSize={}", resultSize);
+        } catch (SQLException e) {
+            log.error("db error");
+            throw e;
+        } finally {
+            close(con, ps, null);
+        }
+    }
+
+    public void delete(String memberId) throws SQLException {
+        String sql = "DELETE FROM member WHERE member_id = ?";
+
+        Connection con = null;
+        PreparedStatement ps = null;
+
+        try {
+            con = getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, memberId);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            log.error("db error");
+            throw e;
+        } finally {
+            close(con, ps, null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(stmt);
+        JdbcUtils.closeConnection(con);
+
+    }
+
+    private Connection getConnection() throws SQLException {
+        Connection con = dataSource.getConnection();
+        log.info("get connection={}, class={}", con, con.getClass());
+        return con;
+    }
+
+}
---
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,61 @@
+package hello.jdbc.repository;
+
+import com.zaxxer.hikari.HikariDataSource;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+class MemberRepositoryV1Test {
+
+    MemberRepositoryV1 repository;
+
+//    @BeforeEach
+//    void beforeEach() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(UL, USERNAME, PASSWORD);
+//        repository = new MemberRepositoryV1(dataSource);
+//    }
+
+    @BeforeEach
+    void beforeEach() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        repository = new MemberRepositoryV1(dataSource);
+    }
+
+    @Test
+    void crud() throws SQLException {
+
+        Member member = new Member("memberV102", 10000);
+        repository.save(member);
+
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember = {}", findMember);
+
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        Assertions.assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+
+    }
+
+}
